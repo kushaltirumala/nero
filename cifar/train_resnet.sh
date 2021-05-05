a620f0e7b236b3243cd583050d18cf51ec0ee9ff
Kushal Tirumala <kushaltirumala99@gmail.com>
2021-05-04 18:45:11 -0700

    doing full resnet depth widht training nero

--- cifar/train_resnet.sh
+++
@@ -1,1 +0,0 @@
-env CUDA_VISIBLE_DEVICES=3 python train.py --gpu --batch_norm --task cifar10 --net resnet --depth 10 --width 16 --optimizer nero --momentum 0.0 --beta 0.999 --wd 0.0 --seed 0  --lr 0.01
---
+++ cifar/train_resnet_nero.sh
@@ -0,0 +1,13 @@
+# env CUDA_VISIBLE_DEVICES=3 python train.py --gpu --batch_norm --task cifar10 --net resnet --depth 10 --width 16 --optimizer nero --momentum 0.0 --beta 0.999 --wd 0.0 --seed 0  --lr 0.01
+# env CUDA_VISIBLE_DEVICES=3 python train.py --gpu --batch_norm --task cifar10 --net resnet --depth 10 --width 16 --optimizer nero --momentum 0.0 --beta 0.999 --wd 0.0 --seed 0  --lr 0.01
+#
+
+
+for depth in 50 42 34 26 18 10
+do
+  for width in 16 32 64 128 256
+  do
+    echo "ON DEPTH $depth and WIDTH $width \n"
+    env CUDA_VISIBLE_DEVICES=3 python train.py --gpu --batch_norm --task cifar10 --net resnet --depth $depth --width $width --optimizer nero --momentum 0.0 --beta 0.999 --wd 0.0 --seed 0  --lr 0.01
+  done
+done
