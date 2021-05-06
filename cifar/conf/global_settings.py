6c9cd08778f287edcd991226994a92070926e23b
Kushal Tirumala <kushaltirumala99@gmail.com>
2021-05-06 03:06:25 -0700

    doing 20 epoch run

--- cifar/conf/global_settings.py
+++ cifar/conf/global_settings.py
@@ -16,10 +16,10 @@ CIFAR100_TRAIN_STD = (0.2673342858792401, 0.2564384629170883, 0.2761504713256840
 #CIFAR100_TEST_STD = (0.2682515741720801, 0.2573637364478126, 0.2770957707973042)
 
 #directory to save weights file
-CHECKPOINT_PATH = 'checkpoint'
+CHECKPOINT_PATH = 'checkpoint_20_epoch'
 
 #total training epoches
-EPOCH = 20
+EPOCH = 21
 MILESTONES = [100,150,180]
 
 #initial learning rate
@@ -29,7 +29,7 @@ MILESTONES = [100,150,180]
 TIME_NOW = datetime.now().strftime('%A_%d_%B_%Y_%Hh_%Mm_%Ss')
 
 #tensorboard log dir
-LOG_DIR = 'runs'
+LOG_DIR = 'runs_20_epoch'
 
 #save weights file per SAVE_EPOCH epoch
 SAVE_EPOCH = 5
