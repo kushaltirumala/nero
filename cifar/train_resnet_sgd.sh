65f40d981ce28eb488ac0fa7491317685d2e2f5a
Kushal Tirumala <kushaltirumala99@gmail.com>
2021-05-09 13:42:32 -0700

    doing sgd run part (1)

--- cifar/train_resnet_sgd.sh
+++ cifar/train_resnet_sgd.sh
@@ -12,11 +12,11 @@
 #   done
 # done
 
-for depth in 34
+for depth in 50 10
 do
-  for width in 128
+  for width in 16 32 64 128 256
   do
     echo "ON DEPTH $depth and WIDTH $width \n"
-    env CUDA_VISIBLE_DEVICES=2 python train.py --gpu --batch_norm --task cifar10 --net resnet --depth $depth --width $width --optimizer sgd --momentum 0.0 --beta 0.999 --wd 0.0 --seed 0  --lr 0.001 --warm 0
+    env CUDA_VISIBLE_DEVICES=0 python train.py --gpu --batch_norm --task cifar10 --net resnet --depth $depth --width $width --optimizer sgd --momentum 0.0 --beta 0.999 --wd 0.0 --seed 0  --lr 0.001 --warm 0
   done
 done
