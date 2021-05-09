ecef564f7d788adaa6d45770b4fb24c234d87099
Kushal Tirumala <kushaltirumala99@gmail.com>
2021-05-08 19:46:01 -0700

    doing normal run 0.01 lr for all params; no lr scheduling

--- cifar/train_mnist_regularizer.sh
+++ cifar/train_mnist_regularizer.sh
@@ -1,1 +1,1 @@
-env CUDA_VISIBLE_DEVICES=3 python train.py --batch_norm --gpu --task mnist --net resnet50 --optimizer nero --momentum 0.0 --beta 0.999 --wd 0.0 --seed 0  --lr 0.01 --gainlr 10.0 --gamma 0.2 --warm 0
+env CUDA_VISIBLE_DEVICES=0 python train.py --batch_norm --gpu --task mnist --net resnet50 --optimizer nero --momentum 0.0 --beta 0.999 --wd 0.0 --seed 0  --lr 0.01 --gainlr 0.01 --gamma 0 --warm 0
